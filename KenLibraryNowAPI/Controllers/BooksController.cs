43a7ad014c28f85e43f4cc47f52d4ca4dfa72496
STUDENT 29 <STUDENT 29@DESKTOP-9G47GDA>
2026-05-02 17:57:44 +0800

    Add project files.

---
+++ KenLibraryNowAPI/Controllers/BooksController.cs
@@ -0,0 +1,134 @@
+﻿using KenLibraryNowAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KenLibraryNowAPI.Controllers
+{
+    [Route("api/v1/book")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static List<Book> books = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Flowers of Algernon",
+                Author = "Daniel Keyes",
+                Genre = "Science Fiction",
+                Available = true,
+                PublishedYear = 1959,
+
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Weight of Ink",
+                Author = "Rachel Kadish",
+                Genre = "Historical Fiction",
+                Available = true,
+                PublishedYear = 2017,
+
+            }
+        };
+
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            return Ok(new
+            {
+                status = "success",
+                data = books,
+                message = "books retrieved"
+
+            });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetId(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return NotFound(new
+                {
+                    status = "error",
+                    data = book,
+                    message = "Book not found."
+                });
+            }
+            return Ok(new
+            {
+                status = "success",
+                data = books,
+                message = "Books retrieved."
+
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Book newBook)
+        {
+            newBook.Id = books.Count + 1;
+            books.Add(newBook);
+            return CreatedAtAction(nameof(GetId),
+                new { id = newBook.Id },
+                new
+                {
+                    status = "Success",
+                    data = newBook,
+                    message = "Book Created"
+                });
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Book updateBook)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not found."
+                });
+
+            book.Title = updateBook.Title;
+            book.Author = updateBook.Author;
+            book.Genre = updateBook.Genre;
+            book.Available = updateBook.Available;
+            book.PublishedYear = updateBook.PublishedYear;
+
+            return Ok(new
+            {
+                status = "success",
+                data = books,
+                message = "Book updated."
+
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not found."
+                });
+
+            books.Remove(book);
+            return Ok(new
+            {
+                status = "success",
+                data = books,
+                message = "Book deleted."
+
+            });
+        }
+    }
+}
---
+++ KenLibraryNowAPI/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://+:{port}");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
+
