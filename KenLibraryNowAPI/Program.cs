7b9ec3fbe8d3dd69ffda633151ca4a9e1fec01c1
Kenzo <calingacionken@gmail.com>
2026-05-09 18:21:28 +0800

    Enable Swagger in all environments
    Always enable Swagger and Swagger UI for API documentation.

--- KenLibraryNowAPI/Program.cs
+++ KenLibraryNowAPI/Program.cs
@@ -14,13 +14,11 @@ var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://+:{port}");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+
+
 app.MapControllers();
 app.Run();
 
